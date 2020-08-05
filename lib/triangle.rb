3028cc6a1c5300a901d308f5d76e9a8b5653785d
Ebo Lee <yiboli.coding@gmail.com>
2020-08-05 21:50:36 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -16,7 +16,7 @@ class Triangle
       :equilateral
     elsif @a == @b || @a == @c || @b == @c
       :isosceles
-    elsif @a !== @b || @b != @c || @c != @a
+    elsif @a != @b || @b != @c || @c != @a
       :scalene
     end
   end
