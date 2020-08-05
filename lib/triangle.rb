99661393ee4eba140cc4e89124b0656067b06c5d
Ebo Lee <yiboli.coding@gmail.com>
2020-08-05 21:47:33 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -8,10 +8,6 @@ class Triangle
   end
 
   def kind
-    if @a + @b == @c
-
-    elsif @a <= 0 || @b <= 0 || @c <= 0 || @a + @b <= @c || @a + @c <= @b || @b + @c <= @a
-      raise TriangleError
     elsif @a == @b && @b == @c
       :equilateral
     elsif @a == @b || @a == @c || @b == @c
